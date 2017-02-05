8a666d35d59f672ff9d9094d94eb47ce6bb4dfe4
MrFricker <switzerland@outlook.co.uk>
2017-02-05 23:26:01 +0000

    Class desgined to represent a route a user can take to discover a resource

---
+++ src/Route.php
@@ -0,0 +1,45 @@
+<?php
+namespace DavidFricker\Router;
+
+use DavidFricker\Router\RouteContainer;
+
+/**
+  * A wrapper around a DB driver to expose a uniform interface
+  *
+  * Bassically an abstraction over the complexity of the PDO class, but by design this could wrap any strctured storage mechanism 
+  * A database engine adapter
+  *
+  * @param string $myArgument With a *description* of this argument, these may also
+  *    span multiple lines.
+  *
+  * @return void
+  */
+class Route {
+    // url e.g. example.com/login -> login
+    private $resource_route;
+    
+    // method@class or lambda func - method@namespace\class for namespaced classes
+    private $target;
+    
+    // http method
+    private $http_method;
+    
+    public function __construct($resource_route, $http_method, $target)
+    {
+        $this->resource_route = $resource_route;
+        $this->http_method = $http_method;
+        $this->target  = $target;
+    }
+
+    public function getTarget() {
+      return $this->target;
+    }
+
+    public function getMethod() {
+      return $this->method;
+    }
+
+    public function getResourceRoute() {
+      return $this->resource_route;
+    }
+}
