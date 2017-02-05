17b3e67a75ec6264d475bc5ba6813be2b0850d85
MrFricker <switzerland@outlook.co.uk>
2017-02-05 23:24:19 +0000

    controller based example

--- example/routes.php
+++ example/routes.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '\..\src\Route.php';
 require __DIR__ . '\..\src\RouteContainer.php';
+require __DIR__ . '\..\src\AuthRouteController.php';
 use DavidFricker\Router\RouteContainer;
 
 $RouteContainer = RouteContainer::init();
@@ -11,3 +12,5 @@ $RouteContainer->set('/login', RouteContainer::HTTP_METHOD_GET, function($reques
 	
 	var_dump($request);
 });
+
+$RouteContainer->set('/logout', RouteContainer::HTTP_METHOD_GET, 'getLogoutPage@DavidFricker\Router\AuthRouteController');
