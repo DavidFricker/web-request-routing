7c00475a74fe879b4a080119a7c517c1fe84ffcc
MrFricker <switzerland@outlook.co.uk>
2017-02-07 13:00:53 +0000

    base controller that must always be extended

---
+++ src/BaseRouteController.php
@@ -0,0 +1,27 @@
+<?php
+namespace DavidFricker\Router;
+
+use DavidFricker\DataAbstracter\ContentProvider;
+
+/**
+  * A wrapper around a DB driver to expose a uniform interface
+  *
+  * Bassically an abstraction over the complexity of the PDO class, but by design this could wrap any strctured storage mechanism 
+  * A database engine adapter
+  *
+  * @param string $myArgument With a *description* of this argument, these may also
+  *    span multiple lines.
+  *
+  * @return void
+  */
+class AbstractRouteController {
+    $content_provider;
+
+    public function __construct() {
+       $this->content_provider = ContentProvider::init();
+    }
+
+    public function authCheck() {
+    
+    }
+}
