ff4090d5123f78a0aad08f736e7989a73bc8d845
MrFricker <switzerland@outlook.co.uk>
2017-02-24 20:14:46 +0000

    better example

--- example/routes.php
+++ example/routes.php
@@ -4,14 +4,20 @@ require __DIR__ . '\..\src\Capsule\RouteContainer.php';
 require __DIR__ . '\..\src\Controller\BaseRouteController.php';
 require __DIR__ . '\..\src\Controller\AuthRouteController.php';
 
-use DavidFricker\Router\Capsule\RouteContainer;
+use \DavidFricker\Router\Capsule\RouteContainer;
 
-$RouteContainer = RouteContainer::init();
+$RouteContainer = \DavidFricker\Router\Capsule\RouteContainer::init();
 
-$RouteContainer->set('/logout', RouteContainer::HTTP_METHOD_GET, 'getLogoutPage@DavidFricker\Router\Controller\AuthRouteController');
+$RouteContainer->set('/', RouteContainer::HTTP_METHOD_GET, function($request) {
+	echo 'index';
+});
 
 $RouteContainer->set('/login', RouteContainer::HTTP_METHOD_GET, function($request) {
 	echo 'this is the login page';
 	
 	var_dump($request);
 });
+
+$RouteContainer->set('/logout', RouteContainer::HTTP_METHOD_GET, 'getLogoutPage@DavidFricker\Router\Controller\AuthRouteController');
+
+$RouteContainer->set('/logout/{user_id}', RouteContainer::HTTP_METHOD_GET, 'getLogoutPageWithID@DavidFricker\Router\Controller\AuthRouteController');
